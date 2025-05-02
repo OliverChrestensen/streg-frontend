ab254cd792d66afd7fe8a4c8057c07b6056db641
OliverChrestensen <oliver.chrestensen@hotmail.com>
2025-05-02 11:27:28 +0200

    added backend server url

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -64,7 +64,7 @@ export default function Game() {
   }, [playerName]);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:3001");
+    const newSocket = io("https://streg-backend.onrender.com");
     socketRef.current = newSocket;
 
     newSocket.on("playerList", (players: Player[]) => {
